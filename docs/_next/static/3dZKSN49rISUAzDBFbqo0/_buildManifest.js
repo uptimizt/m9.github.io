593eb65dfc7fe6a4ff23cd707ff3c4bf27476651
Anatoly Yumashev <1852897+uptimizt@users.noreply.github.com>
2022-12-09 15:12:05 +0300

    mvp

---
+++ docs/_next/static/3dZKSN49rISUAzDBFbqo0/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/813f2f0846445cab.css","static/chunks/pages/index-586d36d374027d4f.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
